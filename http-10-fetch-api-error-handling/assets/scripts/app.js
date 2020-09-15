6d58f96f574b7c5cae254cb1ca22a4d0efd20bd9
sandeepghimire77 <sandeepghimire77@gmail.com>
2020-09-15 16:25:25 +0200

    connecting all buttons to functions video 39 and fetch api error handling

--- http-10-fetch-api-error-handling/assets/scripts/app.js
+++ http-10-fetch-api-error-handling/assets/scripts/app.js
@@ -5,30 +5,53 @@ const fetchButton = document.querySelector('#available-posts button');
 const postList = document.querySelector('ul');
 
 function sendHttpRequest(method, url, data) {
-  const promise = new Promise((resolve, reject) => {
-    const xhr = new XMLHttpRequest();
+  // const promise = new Promise((resolve, reject) => {
+  // const xhr = new XMLHttpRequest();
+  // xhr.setRequestHeader('Content-Type', 'application/json');
 
-    xhr.open(method, url);
+  //   xhr.open(method, url);
 
-    xhr.responseType = 'json';
+  //   xhr.responseType = 'json';
 
-    xhr.onload = function() {
-      if (xhr.status >= 200 && xhr.status < 300) {
-        resolve(xhr.response);
+  //   xhr.onload = function() {
+  //     if (xhr.status >= 200 && xhr.status < 300) {
+  //       resolve(xhr.response);
+  //     } else {
+  // xhr.response;
+  //       reject(new Error('Something went wrong!'));
+  //     }
+  //     // const listOfPosts = JSON.parse(xhr.response);
+  //   };
+
+  //   xhr.onerror = function() {
+  //     reject(new Error('Failed to send request!'));
+  //   };
+
+  //   xhr.send(JSON.stringify(data));
+  // });
+
+  // return promise;
+  return fetch(url, {
+    method: method,
+    body: JSON.stringify(data),
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  })
+    .then(response => {
+      if (response.status >= 200 && response.status < 300) {
+        return response.json();
       } else {
-        reject(new Error('Something went wrong!'));
+        return response.json().then(errData => {
+          console.log(errData);
+          throw new Error('Something went wrong - server-side.');
+        });
       }
-      // const listOfPosts = JSON.parse(xhr.response);
-    };
-
-    xhr.onerror = function() {
-      reject(new Error('Failed to send request!'));
-    };
-
-    xhr.send(JSON.stringify(data));
-  });
-
-  return promise;
+    })
+    .catch(error => {
+      console.log(error);
+      throw new Error('Something went wrong!');
+    });
 }
 
 async function fetchPosts() {
